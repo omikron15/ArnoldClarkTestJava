2985338bdb30cc80e3cbb36be6fcd49aef8b9fd1
omikron15 <omikron15@hotmail.co.uk>
2018-05-22 20:06:20 +0100

    Creates PlanetAgeTest with set up and tests for earth age and mercury age

--- src/test/java/PlanetAgeTest.java
+++ src/test/java/PlanetAgeTest.java
@@ -1,4 +1,22 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class PlanetAgeTest {
 
+    @Before
+    public void setUp(){
 
+    }
+
+    @Test
+    public void earthAge() {
+        assertEquals(1.00, PlanetAge.earthAge(31557600), 0.1);
+    }
+
+    @Test
+    public void mercuryAge() {
+        assertEquals(0.24, PlanetAge.mercuryAge(31557600), 0.1);
+    }
 }
